e03605ce96383e0a14156c8d2dc02ee4daefd194
jeffreyhoang <148601460+jeffreyhoang@users.noreply.github.com>
2026-06-03 22:12:03 -0700

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,6 +8,10 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  server: {
+    port: 5173,
+    strictPort: true,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
